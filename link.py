20d0188197a51efb0defca465ba8fb5ffe762c33
YOU-SYUAN <vickie890514@gmail.com>
2023-11-09 21:20:35 +0800

    first version

--- link.py
+++ link.py
@@ -1,3 +1,3 @@
 import oracledb 
-connection = oracledb.connect(user='account', password='password', host='140.117.69.60', port=1521, service_name='ORCLPDB1') 
+connection = oracledb.connect(user='Group2', password='Z2bbVtVnt', host='140.117.69.60', port=1521, service_name='ORCLPDB1') 
 cursor = connection.cursor()
